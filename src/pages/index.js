f90d2f7307ace04423bd85429b76d107d51f7d69
Jait <jai8.jacob@gmail.com>
2018-08-31 01:56:39 +1000

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to my first Gatsby site.</p>
+    <p>My name is Jait Jacob and this to gain entry to the freeCodeCamp online hackthon</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
